4f5bbf2c73dadc12f74a5e826b688c741ff28667
Diaz <diazkautsar77@gmail.com>
2021-04-29 22:21:31 +0700

    config for context

---
+++ context/index.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, FunctionComponent, ReactNode, useContext, useState } from 'react'
+
+type pokemonContextType = {
+  imageDetailUrl: string,
+  changeImageDetailUrl: (url: string) => void,
+}
+
+const pokemonContextDefaultValue: pokemonContextType = {
+  imageDetailUrl: null,
+  changeImageDetailUrl: (url: string) => {},
+}
+
+const PokemonContext = createContext<pokemonContextType>(pokemonContextDefaultValue)
+
+export function usePokemonContext() {
+  return useContext(PokemonContext)
+}
+
+type Props = {
+  children: ReactNode;
+}
+
+export function PokemonProvider({ children }: Props) {
+  const [imageDetailUrl, setImageDetailUrl] = useState('')
+
+  const changeImageDetailUrl = (url: string) => {
+    setImageDetailUrl(url)
+  }
+
+  const value = {
+    imageDetailUrl,
+    changeImageDetailUrl,
+  }
+
+  return (
+    <PokemonContext.Provider value={value}>
+      { children }
+    </PokemonContext.Provider>
+  )
+}
