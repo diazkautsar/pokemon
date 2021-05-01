1d683132b3d91ba9299c91dcf692632fe11c9600
Diaz <diazkautsar77@gmail.com>
2021-05-01 11:57:49 +0700

    add pagination. see all pokemon. loading div

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
+import React, { useState, MouseEvent } from 'react'
 import Head from 'next/head'
-import React from 'react'
-// import styles from '../styles/Home.module.css'
 import HomePage from '../components/HomePage'
-
 import { GetServerSideProps } from 'next'
 import { initializeApollo } from '../libs/apollo'
-
 import { GET_POKEMONS } from '../utils/graphql/queries'
-
+import Loading from '../components/Loading'
 
 
 const gqlVariables = {
-  limit: 10,
+  limit: 25,
   offset: 1,
 };
 
@@ -35,13 +32,72 @@ export const getServerSideProps: GetServerSideProps = async context => {
 }
 
 function Home({ ...pageProps }) {
+  const client = initializeApollo()
+  const [pokemons, setPokemons] = useState(pageProps.pokemons.results)
+  const [offset, setOffset] = useState<number>(gqlVariables.limit)
+  const [limit] = useState<number>(gqlVariables.limit)
+  const [totalPokemon] = useState<number>(pageProps.pokemons.count)
+  const [isAllPokemon, setIsAllPokemon] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const loadMorePokemon = (event: MouseEvent) => {
+    setLoading(true)
+    event.preventDefault()
+    client.query({
+      query: GET_POKEMONS,
+      variables: {
+        limit,
+        offset: offset + 1,
+      }
+    })
+      .then(({ data }) => {
+        const newPokemons = pokemons.concat(data.pokemons.results)
+        setPokemons(newPokemons)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setOffset(offset + 1)
+        setLoading(false)
+      })
+  }
+
+  const seeAllPokemon = (event: MouseEvent) => {
+    setLoading(true)
+    event.preventDefault()
+    client.query({
+      query: GET_POKEMONS,
+      variables: {
+        limit: totalPokemon,
+        offset: 1,
+      }
+    })
+      .then(({ data }) => {
+        setPokemons(data.pokemons.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+      .finally(() => {
+        setIsAllPokemon(true)
+        setLoading(false)
+      })
+  }
+
   return (
     <React.Fragment>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HomePage pokemons={pageProps.pokemons} {...pageProps} />
+      <HomePage
+        pokemons={pokemons}
+        loadMorePokemon={loadMorePokemon}
+        seeAllPokemon={seeAllPokemon}
+        isAllPokemon={isAllPokemon}
+      />
+      { loading && <Loading /> }
     </React.Fragment>
   )
 }
